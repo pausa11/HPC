4437b6a9f197c884aa47bcc35a56a664160e54e5
pausa11 <daniel.toro1@utp.edu.co>
2026-02-24 18:35:36 -0500

    feat: Generate unique, numbered output files for test results to prevent overwriting.

--- testing.sh
+++ testing.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-OUTPUT_FILE=testing.doc
-# Clear the output file before starting (so we don't append to old results)
->$OUTPUT_FILE
+# Find the first unused filename in the format testing_N.doc
+COUNT=1
+while [[ -f "testing_$COUNT.doc" ]]; do
+  ((COUNT++))
+done
+OUTPUT_FILE="testing_$COUNT.doc"
 
 for j in {1..10}; do
   # Inner loop: runs ./output for each matrix size
-  for i in 200 400 600 800 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400; do # FOr the time just this to test, i dont want to explote my pc
-    ./output $i $i >>$OUTPUT_FILE
+  for i in 200 400 600 800 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400 ; do # For the time just this to test, i don't want to explode my pc
+    ./output $i $i >> "$OUTPUT_FILE"
   done
 
-  echo "" >>$OUTPUT_FILE #Add and space after each i loop
-
+  echo "" >> "$OUTPUT_FILE" # Add a space after each i loop
 done
 
 echo "Listo! Los resultados se han guardado en $OUTPUT_FILE"
