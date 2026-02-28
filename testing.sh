ee9b2455f82d3ea7f23c48c1b941e64324e72f9c
Daniel Toro Soto <daniel.toro1@utp.edu.co>
2026-02-28 14:00:15 -0500

    feat: Generate new performance statistics for matrix multiplication with various optimization levels and refine testing script parameters.

--- testing.sh
+++ testing.sh
@@ -12,9 +12,10 @@ while [[ -f "$STATS_DIR/testing_$COUNT.doc" ]]; do
 done
 OUTPUT_FILE="$STATS_DIR/testing_$COUNT.doc"
 
-for j in {1..10}; do
+for j in {1..3}; do
   # Inner loop: runs ./output for each matrix size
-  for i in 200 400 600 800 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400 ; do # For the time just this to test, i don't want to explode my pc
+  # 200 400 600 800 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400 
+  for i in 2000 ; do # For the time just this to test, i don't want to explode my pc
     ./output $i $i >> "$OUTPUT_FILE"
   done
 
